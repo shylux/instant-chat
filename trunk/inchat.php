35049f9304e6c33a2a6a29f907f543f44e815df5
shylux <shylux@ea39465b-fd38-67b0-4d13-f83d4f9c3871>
2011-12-07 15:31:48 +0000

    Version is broken. Have issues with user/users

--- trunk/inchat.php
+++ trunk/inchat.php
@@ -1,8 +1,10 @@
 <?php
+include_once('inchat_db.php');
 
 // Use Memcache for better performance!
 define('INCHAT_MEMCACHE_ENABLED', false);
 
+define('INCHAT_DB_TYPE', 'postgres');
 define('INCHAT_DB_NAME', 'instantchat');
 define('INCHAT_DB_USER', 'instantchatuser');
 define('INCHAT_DB_PW', 'instantpwchat');
@@ -18,7 +20,6 @@ if (!inchat_authenticate()) {
 
 // Check vor valid action
 if (!isset($ic->request->action)) die("No action set");
-
 
 // Handle different actions
 switch ($ic->request->action) {
@@ -57,15 +58,9 @@ function addmessage() {
 	$username = inchat_getusername();
 	$message = $ic->request->message;
 
-	$getuser_query = "SELECT getcreateuser('".mysql_escape_string($username)."');";
-	$user_res = inchat_mysql_query($getuser_query);
-	$user_array = mysql_fetch_array($user_res);
-	$user_id = $user_array[0];
+	$user_id = $ic->db->getUser($username);
 
-	$add_query = "SELECT addmessage($user_id, '".mysql_escape_string($message)."');";
-	$message_id_res = inchat_mysql_query($add_query);
-	$message_id_array = mysql_fetch_array($message_id_res);
-	$message_id = $message_id_array[0];
+	$ic->db->addMessage($user_id, $message);
 	if (INCHAT_MEMCACHE_ENABLED) $ic->memcache->set('current_message_id', $message_id);
 }
 
@@ -80,20 +75,14 @@ function checknewmessage() {
 	}
 
 	$limit = (INCHAT_MAX_DELIVERED_MESSAGES < $ic->request->max_messages) ? INCHAT_MAX_DELIVERED_MESSAGES : $ic->request->max_messages;
-	$query_string = "SELECT message.*, user.name FROM message, user WHERE message.user = user.id AND message.id > ".mysql_escape_string($lastid)." GROUP BY message.id DESC LIMIT ".mysql_escape_string($limit).";";
-	$result = inchat_mysql_query($query_string);
 
-	$result_array = array();
-	while ($row=mysql_fetch_object($result)) {
-		$row->message = htmlspecialchars($row->message);
-		array_push($result_array, $row);
-	}
+	$result_array = $ic->db->getMessages($lastid, $limit); 
 
 	$ic->response->messages = $result_array;
 }
 function wait_for_message_db() {
 	$ic = Inchat::getInstance();
-	while (count_new_messages($ic->request->lastid) == 0) {
+	while (!$ic->db->isNewMsg($ic->request->lastid)) {
 		usleep(100000);
 	}
 }
@@ -104,21 +93,6 @@ function wait_for_message_memcache() {
 	}
 }
 	
-function count_new_messages($index) {
-	$query = inchat_mysql_query("SELECT checknewmessages('".mysql_escape_string($index)."')");
-	$re_array = mysql_fetch_array($query);
-	return $re_array[0];
-}
-
-function inchat_mysql_query($query) {
-	$ic = Inchat::getInstance();
-	$recource = mysql_query($query);
-	if (!$recource) {
-		$ic->response->send_error("MySQL Error: ".mysql_error()." \nQuery: ".$query);
-	}
-	return $recource;
-}
-
 class inchat_response {
 	public $status = 'success';
 	public $message = 'no message';
@@ -161,13 +135,10 @@ class Inchat {
 			$this->memcache = new Memcached('inchat');
 			$this->memcache->addServer('localhost', 11211);
 		}
-
+		
 		// Database
-		$link = mysql_pconnect(INCHAT_DB_HOST, INCHAT_DB_USER, INCHAT_DB_PW);
-		if (!$link) die ("Can't connect to Database-Server: ".mysql_error());
-
-		$this->db = mysql_select_db(INCHAT_DB_NAME, $link);
-		if (!$this->db) die ("Can't change to Database: ".mysql_error());
+		$this->db = Inchat_db::getInstance();
+		$this->db->setup(INCHAT_DB_TYPE, INCHAT_DB_HOST, INCHAT_DB_NAME, INCHAT_DB_USER, INCHAT_DB_PW);
 	}
 	public function get_curr_msg_id() {
 		$inchat_curr_msg_id = $this->memcache->get('current_message_id');
