bb27943e950c35054b81f5237f0b2a618ff957f7
shylux <shylux@ea39465b-fd38-67b0-4d13-f83d4f9c3871>
2011-12-14 08:20:23 +0000

    Working example, without scrollpane

--- example/inchat_example.js
+++ example/inchat_example.js
@@ -9,13 +9,13 @@ $(document).ready(function () {
 function inchat_appendMessage(username, message, date) {
 	var date_obj = Date.parse(date);
 	var date_out = date_obj.toString('[hh:mm]');
-	var message_template = '<tr class="inchat_msg_entry"> <td class="inchat_msg_date">[[INCHAT_DATE]]</td> <td class="inchat_msg_message">[[INCHAT_MESSAGE]]</td> </tr>';
+	var message_template = '<div class="inchat_msg_entry">[[INCHAT_DATE]] [[INCHAT_MESSAGE]]</div>';
 	var message_html = message_template.replace('[[INCHAT_DATE]]', date_out).replace('[[INCHAT_USER]]', username).replace('[[INCHAT_MESSAGE]]', message);
 	$('#inchat_msg_table').append(message_html);
 }
 
 function inchat_removeMessage() {
-	$('#inchat_msg_table tr:first').remove();
+	$('#inchat_msg_table div:first').remove();
 }
 
 function inchat_showError(message) {}
