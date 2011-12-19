2ad4ea25a1c7fb4efe1fb07d518a02e48d6eb2f3
shylux@gmail.com <shylux@gmail.com@ea39465b-fd38-67b0-4d13-f83d4f9c3871>
2011-12-19 12:51:55 +0000

    Working example added.

--- trunk/example/inchat_example.js
+++ trunk/example/inchat_example.js
@@ -1,21 +1,30 @@
 INCHAT_TARGET = "../inchat.php";
-INCHAT_MAX_MESSAGES_DISPLAYED = 5;
+INCHAT_MAX_MESSAGES_DISPLAYED = -1;
+
+var inchat_pane;
 
 $(document).ready(function () {
 	inchat_start();
 	inchat_focus();
+	inchat_pane = $('#inchat_msg_table').jScrollPane({maintainPosition: false}).data('jsp')
+	inchat_pane.scrollToBottom();
 });
 
 function inchat_appendMessage(username, message, date) {
 	var date_obj = Date.parse(date);
 	var date_out = date_obj.toString('[hh:mm]');
-	var message_template = '<div class="inchat_msg_entry">[[INCHAT_DATE]] [[INCHAT_MESSAGE]]</div>';
+	var message_template = '<p class="inchat_msg_entry">[[INCHAT_DATE]] [[INCHAT_MESSAGE]]</p>';
 	var message_html = message_template.replace('[[INCHAT_DATE]]', date_out).replace('[[INCHAT_USER]]', username).replace('[[INCHAT_MESSAGE]]', message);
-	$('#inchat_msg_table').append(message_html);
+
+	var scrollbot = false;
+	if (inchat_pane.getPercentScrolledY() == 100) scrollbot = true;
+	inchat_pane.getContentPane().append(message_html);
+	inchat_pane.reinitialise();
+	inchat_pane.scrollToBottom();
 }
 
 function inchat_removeMessage() {
-	$('#inchat_msg_table div:first').remove();
+	//$('#inchat_msg_table div:first').remove();
 }
 
 function inchat_showError(message) {}
--- trunk/example/sysadm.js
+++ trunk/example/sysadm.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#sys_navigation .sys_sec').bind('click', function() {
 		site = $(this).attr('site');
+		$("title").text($(this).text());
 		$(location).attr("hash", site);
 		$('#sys_frame').attr('src', site);
 
@@ -12,7 +13,7 @@ $(document).ready(function() {
 	$('#sys_navigation .sys_sec').addClass('sys_unselected');
 	var hash = $(location).attr("hash").substr(1);
 	if (hash != "") {
-		$('#sys_navigation div').each(function() {
+		$('#sys_navigation .sys_sec').each(function() {
 			if ($(this).attr('site') == hash) $(this).click();
 		});
 	} else {
@@ -24,8 +25,28 @@ $(document).ready(function() {
 function resize() {
 	var doc = $(document);
 	var navi = $('#sys_navigation');
-	navi.height( doc.height() - (navi.outerHeight(true)-navi.height()) );
-	$('#sys_frame').width( doc.width() - navi.outerWidth(true) -30 );
-	$('#sys_frame').height( doc.height() - 20);
+	var v = viewport();
+
+	// real height (h + padding + border + margin) - content height of navigation bar
+	var mbp = navi.outerHeight(true)-navi.height();
+	$('#sys_frame').height(v['height']);
+	navi.height(v['height'] - mbp);
+
+	var offset = 1;
+	if ($(document).height() > v["height"]) {
+		offset = 20;
+		setTimeout("resize();", 50);
+	}
+	$('#sys_frame').width( v['width'] - navi.outerWidth(true)-offset);
 }
 window.onresize = resize;
+
+function viewport() {
+	var e = window
+	, a = 'inner';
+	if ( !( 'innerWidth' in window ) ) {
+		a = 'client';
+		e = document.documentElement || document.body;
+	}
+	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+}
